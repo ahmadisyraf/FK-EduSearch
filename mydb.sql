6e0ea46f961c10d1672007e39e9fa79022eab9f4
ahmadisyraf <isyrafmagic@gmail.com>
2023-05-29 12:50:07 +0800

    Update: add user table

--- mydb.sql
+++ mydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 28, 2023 at 05:46 PM
+-- Generation Time: May 29, 2023 at 04:49 AM
 -- Server version: 8.0.31
 -- PHP Version: 7.4.33
 
@@ -41,6 +41,30 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`adminid`, `adminFullName`, `adminEmail`, `adminPassword`) VALUES
 (9, 'AHMAD ISYRAF BIN MOHD FAISHAL-ADZHA', 'isyrafmagic@gmail.com', '12345');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int NOT NULL,
+  `userFullName` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `userEmail` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `userPassword` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(60) COLLATE utf8mb4_general_ci NOT NULL,
+  `userUpdateProfileStatus` tinyint(1) DEFAULT NULL,
+  `userCity` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `userState` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `userFullName`, `userEmail`, `userPassword`, `username`, `userUpdateProfileStatus`, `userCity`, `userState`) VALUES
+(1, 'AHMAD ISYRAF BIN MOHD FAISHAL-ADZHA', 'isyrafmagic@gmail.com', '12345', 'isyrafmagic', NULL, NULL, NULL);
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +76,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`adminid`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +90,12 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `adminid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
