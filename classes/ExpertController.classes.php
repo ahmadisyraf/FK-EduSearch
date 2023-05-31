35ff7b7d017a115f4d372e34cf20fbd97203b4a2
Izzahan <izzahan78@gmail.com>
2023-05-31 16:08:35 +0800

    module 3
    class & controller

--- classes/ExpertController.classes.php
+++ classes/ExpertController.classes.php
@@ -3,6 +3,14 @@ class ExpertController extends Expert {
     public function getExpertController($email, $password) {
         return $this->getExpert($email, $password);
     }
+
+    public function getAllExpertController() {
+        return $this->getAllExpert();
+    }
+
+    public function getExpertByEmailController($email) {
+        return $this->getExpertByEmail($email);
+    }
 }
 
 ?>
