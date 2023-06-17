25aa740dc73b1e9289c834c6f51ba8726393cee4
fqhaif01 <Fqhasif01@gmail.com>
2023-06-17 14:36:09 +0800

    update

--- home.php
+++ home.php
@@ -5,15 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
     <?php
 
@@ -28,7 +25,7 @@
     if (isset($_POST['submit'])) {
         $_COOKIE['user_data'];
         $user_cookie = json_decode($_COOKIE['user_data'], true);
-    
+
         $uid = $user_cookie['uid'];
         $topic = $_REQUEST['topic'];
         $content = $_REQUEST['content'];
@@ -43,7 +40,7 @@
         if ($category == "bcs" || $category == "bcg" || $category == "bcn") {
             $postController = new PostController();
             $result = $postController->insertPostController($uid, $topic, $content, $category, $image);
-        
+
             if (!$result) {
                 $show_error = true;
                 $show_message = "Failed to insert post data";
@@ -55,11 +52,29 @@
 
     $_SESSION['logged_out'];
 
-    
-
     if ($_SESSION['logged_out'] == true) {
         header("Location: index.php");
     }
+    if (isset($_POST['addcomplaint'])) {
+        $_COOKIE['user_data'];
+        $user_cookie = json_decode($_COOKIE['user_data'], true);
+
+        $userid = $user_cookie['userid'];
+        $complaintDate = $_REQUEST['complaintDate'];
+        $complaintType = $_REQUEST['complaintType'];
+        $complaintDescription = $_REQUEST['complaintDescription'];
+
+        $complaintController = new complaintController();
+        $resultcomplaint = $complaintController->insertComplaintController($userid, $complaintDate, $complaintType, $complaintDescription);
+
+        if (!$resultcomplaint) {
+            $show_error = true;
+            $show_message = "Failed to insert post data";
+        } else {
+            $show_success = true;
+        }
+    }
+
     ?>
 
     <?php include "components/navigation.php"; ?>
@@ -69,8 +84,7 @@
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                         Category
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -86,8 +100,7 @@
         <div class="card mt-3" style="width: 40%; margin-bottom: 100px">
             <div class="card-body">
                 <div class="d-flex">
-                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                        class="rounded-circle me-3" style="width: 40px; height: 40px;" alt="Avatar" />
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 40px; height: 40px;" alt="Avatar" />
                     <div class="row">
                         <h6 class="inline my-0">AHMAD ISYRAF BIN MOHD FAISHAL-ADZHA</h6>
                         <p><u>Software Engineering</u>. Posted on May 27</p>
@@ -106,8 +119,7 @@
                         <button class="btn btn-icon btn-transparent btn-like" type="button">
                             <i class="bi bi-heart"></i>
                         </button>
-                        <button class="btn btn-icon btn-transparent btn-comment" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#commentSection" aria-expanded="false" aria-controls="commentSection">
+                        <button class="btn btn-icon btn-transparent btn-comment" type="button" data-bs-toggle="collapse" data-bs-target="#commentSection" aria-expanded="false" aria-controls="commentSection">
                             <i class="bi bi-chat"></i>
                         </button>
                     </div>
@@ -118,8 +130,7 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                                    class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
                                 <div>
                                     <div class="row">
                                         <h6 class="inline my-0">Expert Name</h6>
@@ -130,59 +141,47 @@
                                         of Laravel include...</p>
                                 </div>
                             </div>
-                            <button class="btn btn-icon btn-transparent btn-report position-absolute top-0 end-0"
-                                data-bs-target="#exampleModal" data-bs-toggle="modal" type="button">
+                            <button class="btn btn-icon btn-transparent btn-report position-absolute top-0 end-0" data-bs-target="#exampleModal" data-bs-toggle="modal" type="button">
                                 <i class="bi bi-exclamation-circle"></i>
                             </button>
 
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                aria-hidden="true">
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h1 class="modal-title fs-5" id="exampleModalLabel">Complaint</h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
+                                        <form action="home.php" method="post"></form>
                                         <div class="modal-body">
-                                            <div class="mb-3">
-                                                <label for="exampleFormControlInput1" class="form-label d-flex">Email
-                                                    address</label>
-                                                <input type="email" class="form-control" id="exampleFormControlInput1"
-                                                    placeholder="name@example.com">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleFormControlInput2"
-                                                    class="form-label d-flex">Description</label>
-                                                <input type="text" class="form-control" id="exampleFormControlInput2"
-                                                    placeholder="....">
-                                            </div>
+                                            <form action="/action_page.php">
+                                                <label for="example">Date</label>
+                                                <input style="width:150px" type="datetime-local" id="complaintDate" name="complaintDate">
+                                            </form>
+
+                                            <br>
                                             <label for="exampleFormControlInput4" class="form-label d-flex">Complaint
                                                 Type</label>
-                                            <select class="form-select" aria-label="Default select example">
+                                            <select class="form-select" aria-label="Default select example" name="complaintType" id="complaintType">
                                                 <option selected>Open this select menu</option>
-                                                <option value="1">Unsatisfied Expert's Feedback</option>
-                                                <option value="2">Wrongly Assigned Research Area</option>
-                                                <option value="3">Other</option>
+                                                <option value="Unsatisfied Expert's Feedback">Unsatisfied Expert's Feedback</option>
+                                                <option value="Wrongly Assigned Research Area">Wrongly Assigned Research Area</option>
+                                                <option value="Other">Other</option>
                                             </select>
                                             <br>
-                                            <form action="/action_page.php">
-                                                <label style="" ; for="example">Date</label>
-                                                <input style="width:150px" type="datetime-local" id="birthdaytime"
-                                                    name="birthdaytime">
-                                            </form>
+                                            <div class="mb-3">
+                                                <label for="exampleFormControlInput2" class="form-label d-flex">Description</label>
+                                                <input type="text" class="form-control" id="complaintDescription" name="complaintDecription" placeholder="....">
+                                            </div>
+
                                             <br>
-                                            <form action="/action_page.php">
-                                                <label for="appt">Select a time:</label>
-                                                <input type="time" id="appt" name="appt">
-                                            </form>
+
                                         </div>
                                         <div class="modal-footer">
-                                            <a href="#" style="color:white; background-color: #080202; width:100px"
-                                                class="btn">Back</a>
-                                            <a href="#" style="color:white; background-color: #080202; width:160px"
-                                                class="btn">Add Complaint</a>
+                                            <a href="home.php" style="color:white; background-color: #080202; width:100px" class="btn">Back</a>
+                                            <a href="usercomplaint.php" style="color:white; background-color: #080202; width:160px" class="btn" name="addcomplaint">Add Complaint</a>
                                         </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
@@ -209,14 +208,11 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                                    class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
                                 <div>
                                     <p class="my-0"><b>John Doe</b></p>
                                     <p>This is a great question. Laravel offers several advantages such as...</p>
-                                    <button class="btn btn-sm btn-transparent btn-reply" type="button"
-                                        data-bs-toggle="collapse" data-bs-target="#replySection-1" aria-expanded="false"
-                                        aria-controls="replySection-1">
+                                    <button class="btn btn-sm btn-transparent btn-reply" type="button" data-bs-toggle="collapse" data-bs-target="#replySection-1" aria-expanded="false" aria-controls="replySection-1">
                                         <b>Reply 1></b>
                                     </button>
                                 </div>
@@ -225,8 +221,7 @@
                         <div class="collapse" id="replySection-1">
                             <div class="card-body">
                                 <div class="d-flex">
-                                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                                        class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
+                                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
                                     <div>
                                         <p class="my-0"><b>Your Name</b></p>
                                         <p>Your reply to the comment goes here.</p>
@@ -239,8 +234,7 @@
                     <div class="card mt-3">
                         <div class="card-body">
                             <div class="d-flex">
-                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                                    class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
+                                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 30px; height: 30px;" alt="Avatar" />
                                 <div>
                                     <p class="my-0"><b>Jane Smith</b></p>
                                     <p>I agree with John. Laravel's features like...</p>
@@ -261,8 +255,7 @@
                     <h5 class="card-title">New Post</h5>
                 </div>
                 <div class="d-flex">
-                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                        class="rounded-circle me-3" style="width: 40px; height: 40px;" alt="Avatar" />
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="rounded-circle me-3" style="width: 40px; height: 40px;" alt="Avatar" />
                     <div class="row mb-3">
                         <small><b>AHMAD ISYRAF BIN MOHD FAISHAL-ADZHA</b></small>
                     </div>
--- usercomplaint.php
+++ usercomplaint.php
@@ -6,23 +6,105 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Complaints</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #f8f9fa;
+            color: #343a40;
+        }
+
+        .card-header {
+            background-color: #343a40;
+            color: #fff;
+        }
+
+        .nav-link {
+            color: #fff;
+        }
+
+        .card-body {
+            background-color: #fff;
+            border: 1px solid #dee2e6;
+            border-radius: 0.25rem;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #dee2e6;
+        }
+
+        th {
+            background-color: #f8f9fa;
+        }
+
+        .btn-back {
+            color: #fff;
+            background-color: #080202;
+            width: 100px;
+            text-align: center;
+            display: block;
+            margin: 20px auto;
+            padding: 8px 16px;
+            text-decoration: none;
+            border-radius: 4px;
+        }
+
+        .alert-primary {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        .alert-warning {
+            background-color: #ffc107;
+            color: #212529;
+        }
+
+        .alert-success {
+            background-color: #28a745;
+            color: #fff;
+        }
+    </style>
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
-
     <?php include "components/navigation.php"; ?>
     <div class="d-flex flex-column justify-content-center align-item-center vh-100" style="padding-left: 100px; padding-right: 100px">
-        <h3 class="mt-5">My Complaints</h3>
-        <div class="card text-center" >
-            <div class="card-header" >
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <?php
+        include "config/autoload.php";
+
+        $complaint = new ComplaintController();
+        $user = new UserController();
+
+        $user_data = json_decode($_COOKIE['user_data'], true);
+        $userid = $user_data['uid'];
+
+        $userid;
+        $complaintDate;
+        $complaintType;
+        $complaintid;
+
+        $complaint = $complaint->getComplaintController($userid);
+
+
+
+
+        ?>
+
+
+        <h3 class="mt-5"><img style="width:150px; height:150px" src="public/undraw_Things_to_say_re_jpcg.png" height="30"> My Complaints</h3>
+        <div class="card text-center">
+            <div class="card-header">
                 <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item" >
-                        <a class="nav-link active" aria-current="true" href="#" >All Complaint</a>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="true" href="#"><i class="fas fa-list"></i> All Complaints</a>
                     </li>
                 </ul>
             </div>
@@ -30,57 +112,57 @@
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">ComplaintID</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Complaint</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Time</th>
-                            <th scope="col"style="width:20px">Status</th>
+                            <th>ComplaintID</th>
+                            <th style="width:10px;">User ID</th>
+                            <th>Complaint</th>
+                            <th>Date</th>
+                            <th style="width:20px">Status</th>
+                            <td></td>
                         </tr>
                     </thead>
                     <tbody>
+                        <?php
+                        if ($complaint) {
+                            if ($complaint->num_rows > 0) {
+                                while ($row = $complaint->fetch_assoc()) {
+                                    $complaintid = $row['complaintid'];
+                                    $userid = $row['userid'];
+                                    $complaintDate = $row['complaintDate'];
+                                    $complaintType = $row['complaintType'];
+                                    echo '
                         <tr>
-                            <th scope="row"style="width:10px">1</th>
-                            <td>...</td>
-                            <td>...</td>
-                            <td>...</td>
-                            <td>...</td>
+                            <th scope="row" style="width:40px">'.$complaintid.'</th>
+                            <td style="width:10px;">'.$userid.'</td>
+                            <td>'.$complaintType.'</td>
+                            <td>'.$complaintDate.'</td>
                             <td>
                                 <div class="alert alert-primary" role="alert">
                                     In-Investigation
                                 </div>
                             </td>
-                        </tr>
-                        <tr>
-                            <th scope="row" style="width:10px">2</th>
-                            <td>...</td>
-                            <td>...</td>
-                            <td>...</td>
-                            <td>...</td>
                             <td>
-                                <div class="alert alert-warning" role="alert">
-                                    In-Hold
+                                <div class="dropdown">
+                                    <button style="color:black;" class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="complaintdetails.php?userid=' . $userid . '">View</a></li>
+                                        <li><a class="dropdown-item" href="#">Delete</a></li>
+                                    </ul>
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <th scope="row" style="width:10px">3</th>
-                            <td>....</td>
-                            <td>....</td>
-                            <td>....</td>
-                            <td>...</td>
-                            <td>
-                                <div class="alert alert-success" role="alert">
-                                    Resolved
-                                </div>
-                            </td>
-                        </tr>
+                                ';
+                                }
+                            }
+                        } ?>
+                        
                     </tbody>
                 </table>
-                <a href="#" style="color:white; background-color: #080202; width:100px" class="btn" >Back</a>
+                <a href="#" class="btn-back">Back</a>
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
